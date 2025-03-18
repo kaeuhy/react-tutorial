b6024e387fb769cb5b0782eb3b2dd2eb06703c04
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-19 01:29:56 +0900

    MyHello.jsx

---
+++ my-react/src/MyHello.jsx
@@ -0,0 +1,5 @@
+export default function MyHello(props) {
+    return (
+        <div>안녕하세요, {props.myName}님!</div>
+    );
+}
--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -4,12 +4,12 @@ import { createRoot } from 'react-dom/client'
 // 전역 스타일 가져오기
 import './index.css'
 // 앱별 코드가져오기
-import App from './App.jsx'
+import MyHello from './MyHello.jsx'
 
 // 리액트 앱 렌더링
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyHello myName="홍길동" />
   </StrictMode>,
 )
 
