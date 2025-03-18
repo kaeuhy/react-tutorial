6cf7369e039b375aaed1877ac01165feb6904ad3
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-19 02:22:29 +0900

    EventBasic.jsx

--- my-react/src/MyHello.jsx
+++ my-react/src/MyHello.jsx
@@ -1,5 +1,6 @@
-export default function MyHello(props) {
-    return (
-        <div>안녕하세요, {props.myName}님!</div>
-    );
-}
+// export default function MyHello({myName}) {
+//     return (
+//         <div>안녕하세요, {myName}님!</div>
+//     );
+// }
+//
--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -1,15 +1,11 @@
-// React 라이브러리 가져오기
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// 전역 스타일 가져오기
 import './index.css'
-// 앱별 코드가져오기
-import MyHello from './MyHello.jsx'
+import EventBasic from './EventBasic'
 
-// 리액트 앱 렌더링
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MyHello myName="홍길동" />
+      <EventBasic type={"time"} />
   </StrictMode>,
 )
 
