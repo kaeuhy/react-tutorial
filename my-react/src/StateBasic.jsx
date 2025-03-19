44c594ac9983d4123e9c07c71515eb6d2bfd195a
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-19 11:32:50 +0900

    .jsx

--- my-react/src/StateBasic.jsx
+++ my-react/src/StateBasic.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 
 export default function StateBasic({ init }) {
+    /* Props로 State 초기화 */
     const [count, setCount] = useState(init);
+    console.log(`count is ${count}.`);
+    /* [ 카운트 ] 버튼 클릭 시 카운트 값 증가 */
     const handleClick = () => setCount(count + 1);
 
     return (
