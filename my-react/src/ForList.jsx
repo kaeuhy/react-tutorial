e63b1524681bba333e944426164b0ad3a0730198
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-21 17:10:16 +0900

    ForList modified

--- my-react/src/ForList.jsx
+++ my-react/src/ForList.jsx
@@ -6,15 +6,15 @@ export default function ForList({ src }) {
         // 도서 정보(src 속성)를 <dt>/<dd> 목록으로 정형화
         <dl>
             {src.map(elem => (
-                <>
+                <React.Fragment key={elem.isbn}>
                     <dt>
                         <a href={`https://wikibook.co.kr/images/cover/s/${elem.isbn}.jpg`}>
                             {elem.title} ({elem.price}원)
                         </a>
                     </dt>
                     <dd>{elem.summary}</dd>
-                </>
-            ))}
+                </React.Fragment>
+                ))}
         </dl>
     );
 }
