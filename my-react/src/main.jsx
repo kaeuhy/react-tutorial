5549526f16a97966dec5be4c04e3fb4f4098ae85
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-25 20:54:40 +0900

    State-study-From.js

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -4,8 +4,9 @@ import './index.css'
 import books from './books'
 import ForFilter from "./ForFilter"
 import SelectStyle from './SelectStyle';
+import {Form} from "./Form.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <SelectStyle src={books} />
+    <Form/>
 );
 
