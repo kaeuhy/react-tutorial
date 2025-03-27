d20cd816367a69fe88d3e935364a397f0ba2d43a
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-27 10:33:11 +0900

    TitledPanel.jsx

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -6,11 +6,16 @@ import ForFilter from "./ForFilter"
 import SelectStyle from './SelectStyle';
 import {Form} from "./Form.jsx";
 import StyledPanel from "./StyledPanel.jsx";
+import TitledPanel from "./TitledPanel.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <StyledPanel>
+    <TitledPanel
+        title={
         <p>회원 모집 중!</p>
-        <p>위키북스 프로젝트에 오신 것을 환영합니다 ! !</p>
-    </StyledPanel>
+        }
+    body={
+        <p>위키북스 프로젝트에 오신 것을 환영합니다! !</p>
+    }>
+    </TitledPanel>
 );
 
