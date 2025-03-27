6c18006c5998eb7dd9b3d63676ba96dff41af256
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-27 10:20:08 +0900

    StyledPanel.jsx

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -5,8 +5,12 @@ import books from './books'
 import ForFilter from "./ForFilter"
 import SelectStyle from './SelectStyle';
 import {Form} from "./Form.jsx";
+import StyledPanel from "./StyledPanel.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <Form/>
+    <StyledPanel>
+        <p>회원 모집 중!</p>
+        <p>위키북스 프로젝트에 오신 것을 환영합니다 ! !</p>
+    </StyledPanel>
 );
 
