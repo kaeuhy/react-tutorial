7ec8b22e5ed908523e31e4df02de80fd9ec8fc7e
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-29 00:44:30 +0900

    objectOf

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -12,6 +12,6 @@ import MyHello from "./MyHello.jsx";
 import TypeProp, {Member} from "./TypeProp.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <TypeProp prop4={[333, '홍길동']} />
+    <TypeProp prop5={{ '홍길동: 15', '이순신': '삼십' }} />
 );
 
