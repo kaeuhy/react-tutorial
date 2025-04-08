6dd31b1f7d6ba8a0ac7c925e13df35c12d4ad78b
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-04-08 17:14:47 +0900

    StateTodo.jsx 4-2-9

--- my-react/src/StateTodo.jsx
+++ my-react/src/StateTodo.jsx
@@ -3,46 +3,60 @@ import './StateTodo.css';
 
 let maxId = 0;
 export default function StateTodo() {
-    const[title, setTitle] = useState('');
-    const[todo, setTodo] = useState([]);
-    const handleChangeTitle = e => {
-        setTitle(e.target.value);
-    };
+  const [title, setTitle] = useState('');
+  const [todo, setTodo] = useState([]);
 
-    const handleDone = e => {
-        setTodo(todo.map(item => {
-            if(item.id === Number(e.currentTarget.id)) {
-                return {
-                    ...item,
-                    isDone: true
-                };
-            } else {
-                return item;
-            }
-        }));
-    };
+  const handleChangeTitle = e => {
+    setTitle(e.target.value);
+  };
 
+  const handleClick = () => {
+    setTodo([
+      ...todo,
+      {
+        id: ++maxId,
+        title,
+        created: new Date(),
+        isDone: false
+      }
+    ]);
+  };
 
-    return (
-        <div>
-            <label>
-                <input type="text" name="title"
-                       value={title} onChange={handleChangeTitle} />
-            </label>
+  const handleDone = e => {
+    setTodo(todo.map(item => {
+      if (item.id === Number(e.target.dataset.id)) {
+        return {
+          ...item,
+          isDone: true
+        };
+      } else {
+        return item;
+      }
+    }));
+  };
+
+  return (
+    <div>
+      <label>
+        해야 할 일:
+        <input type="text" name="title"
+          value={title} onChange={handleChangeTitle} />
+      </label>
+      <button type="button"
+        onClick={handleClick}>추가하기</button>
+      {/* 할 일을 목록으로 정리하기 */}
+
+      <ul>
+        {todo.map(item => (
+          <li key={item.id}
+            className={item.isDone ? 'done' : ''}>
+            {item.title}
             <button type="button"
-                    onClick={handleClick}>추가하기</button>
-            <hr />
-            <ul>
-                {todo.map(item => (
-                    <li key={item.id}
-                    className={item.isDone ? 'done' : ''}>
-                        {item.title}
-                        <button type="button"
-                                onClick={handleDone} data-id={item.id}>완료
-                            </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+              onClick={handleDone} data-id={item.id}>완료
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
