287bdc3af3ec88a7e27991a39c1c45723d8392f0
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-04-29 16:22:15 +0900

    add

--- src/ThrowPromise.jsx
+++ src/ThrowPromise.jsx
@@ -1,3 +1,13 @@
-export default function ThrowPromise() {
-    throw new Promise((resolve, reject) => { });
+let flag = false;
+
+export default function THrowPromise() {
+    if (flag) {
+        return <p>올바르게 표시되었다.</p>;
+    }
+    throw new Promise((resolve, reject) => {
+        setTimeout(() => {
+            flag = true;
+            resolve('Susccess!!');
+        }, 3000);
+    });
 }
