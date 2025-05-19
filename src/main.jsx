da4c9ec80191848975bb790ad3b7935070a03de7
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-19 17:20:11 +0900

    StateEffect.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import {createRoot} from 'react-dom/client'
 import './global.css'
 import PortalExample from "./PortalExample.jsx";
 import NoPortalExample from "./NoPortalExample.jsx";
+import StateEffect from "./StateEffect.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
-            <div id="dialog"></div>
-            <PortalExample/>
-            <NoPortalExample/>
+            <StateEffect init={0} />
         </div>
     </>
 );
