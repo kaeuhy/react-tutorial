33195a4f4f980f25b4036664512b2d15f11702f8
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-23 19:19:20 +0900

    useRef를 사용한 HookRefNg.jsx

--- src/HookRefNg.jsx
+++ src/HookRefNg.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function HookRefNg() {
-    let id = null;
+    const id = useRef(null);
     const [count, setCount] = useState(0);
 
     const handleStart = () => {
-        if (id === null) {
-            id = setInterval(() => setCount(c => c + 1), 1000);
+        if (id.current === null) {
+            id.current = setInterval(() => setCount(c => c + 1), 1000);
         }
     };
 
     const handleEnd = () => {
-        clearInterval(id);
-        id = null;
+        clearInterval(id.current);
+        id.current = null;
     };
 
     return (
