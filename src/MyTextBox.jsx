f8d22d30773cac6d5917d49bbce1f92b33a02936
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-23 19:48:25 +0900

    forwardRef를 사용한 MyTextBox.jsx

--- src/MyTextBox.jsx
+++ src/MyTextBox.jsx
@@ -1,8 +1,10 @@
-export default function MyTextBox({ label, ref }) {
-    return (
-        <label>
-            {label} :
-            <input type="text" size="15" ref={ref} />
-        </label>
-    );
-}
+import { forwardRef } from 'react';
+
+const MyTextBox = forwardRef(({ label }, ref) => (
+    <label>
+        {label} :
+        <input type="text" size="15" ref={ref} />
+    </label>
+));
+
+export default MyTextBox;
