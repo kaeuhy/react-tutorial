846535d23307ebfc17bec4d33fdff78c1ed4364a
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-26 23:42:09 +0900

    개선된 HookReducerUp.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import {createRoot} from 'react-dom/client'
 import './global.css'
 import HookCallbackRef from "./HookCallbackRef.jsx";
 import HookReducer from "./HookReducer.jsx";
+import HookReducerUp from "./HookReducerUp.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
-            <HookReducer init={0}/>
+            <HookReducerUp init={0}/>
         </div>
     </>
 );
