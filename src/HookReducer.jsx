f947b41c00383750e42b49d3337c912b9f85f5ac
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-26 23:15:32 +0900

    useReducer 훅을 이용한 HookReducer.jsx 실습입니다.

--- src/HookReducer.jsx
+++ src/HookReducer.jsx
@@ -2,6 +2,28 @@ import { useReducer } from "react";
 
 export default function HookReducer({ init }) {
     const [state, dispatch] = useReducer(
+        (state, action) => {
+            switch (action.type) {
+                case 'update':
+                    return { count: state.count + 1 };
+                    default:
+                        return state;
+            }
+        },
 
-    )
+        {
+            count: init
+        }
+    );
+
+    const handleClick = () => {
+        dispatch({ type: 'update' });
+    };
+
+    return (
+        <>
+            <button onClick={handleClick}>카운트</button>
+            <p>{state.count}번 클릭되었습니다.</p>
+        </>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import {createRoot} from 'react-dom/client'
 import './global.css'
 import HookCallbackRef from "./HookCallbackRef.jsx";
+import HookReducer from "./HookReducer.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
-            <HookCallbackRef />
+            <HookReducer init={0}/>
         </div>
     </>
 );
