97529b07044a513636d8fcde5657ec4fa6557920
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-26 15:46:01 +0900

    HookCallbackRef.jsx

---
+++ src/HookCallbackRef.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function HookCallbackRef() {
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    const address = useRef(null);
+
+    useEffect(() => {
+        if (address.current) {
+            address.current.focus();
+        }
+    }, [show]);
+
+    return (
+        <>
+        <div>
+            <label htmlFor="name">이름:</label>
+            <input id="name" type="text" />
+        </div>
+            <div>
+                <label htmlFor="email">이메일 주소:</label>
+                <input id="email" type="text" />
+                <button onClick={handleClick}>확장 표시</button>
+            </div>
+            {show &&
+                <div>
+                    <label htmlFor="address">주소:</label>
+                <input id="address" type="text" ref={address} />
+                </div>
+            }
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import {createRoot} from 'react-dom/client'
 import './global.css'
-import PortalExample from "./PortalExample.jsx";
-import NoPortalExample from "./NoPortalExample.jsx";
-import StateEffect from "./StateEffect.jsx";
-import HookTimer from "./HookTimer.jsx";
-import HookEffect from "./HookEffect.jsx";
-import HookRefNg from "./HookRefNg.jsx";
+import HookCallbackRef from "./HookCallbackRef.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
-            <HookRefNg />
+            <HookCallbackRef />
         </div>
     </>
 );
