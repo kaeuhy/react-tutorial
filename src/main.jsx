ebefffdc754639174f1b0e91d237575a47f70b00
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-29 19:00:22 +0900

    mod

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,5 @@
 import {createRoot} from 'react-dom/client'
 import './global.css'
-import HookCallbackRef from "./HookCallbackRef.jsx";
-import HookReducer from "./HookReducer.jsx";
-import HookReducerUp from "./HookReducerUp.jsx";
-import HookReducerInit from "./HookReducerInit.jsx";
-import HookContext from "./HookContext.jsx";
-import HookThemeButton from "./HookThemeButton.jsx";
-import MyThemeProvider from "./MyThemeProvider.jsx";
 import RecoilCounter from "./RecoilCounter.jsx";
 import {RecoilRoot} from "recoil";
 
@@ -19,4 +12,3 @@ createRoot(document.getElementById('root')).render(
         </div>
     </>
 );
-
