25e0c4fbf2eae77c0f0ee70ed02c8eef8142def5
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-05-29 17:42:32 +0900

    State를 초기화하기 위한 useResetRecoilState 함수를 사용하여 [Reset] 버튼 추가

--- src/RecoilCounter.jsx
+++ src/RecoilCounter.jsx
@@ -1,8 +1,9 @@
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useResetRecoilState } from 'recoil';
 import { counterAtom } from './store/atom.jsx';
 
 export default function RecoilCounter() {
     const [counter, setCounter] = useRecoilState(counterAtom);
+    const resetCounter = useResetRecoilState(counterAtom);
 
     const handleClick = () => {
         setCounter(c => c + 1);
@@ -11,6 +12,7 @@ export default function RecoilCounter() {
     return (
         <>
             <button onClick={handleClick}>카운트</button>
+            <button onClick={() => resetCounter()}>리셋</button>
             <p>{counter}번 클릭되었습니다. </p>
         </>
     );
