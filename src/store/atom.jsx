90b5b7cd3f5124343bae9f57ac378865f846e552
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-03 00:26:53 +0900

    mod

--- src/store/atom.jsx
+++ src/store/atom.jsx
@@ -4,3 +4,14 @@ export const counterAtom = atom({
     key: 'counterAtom',
     default: 0,
 });
+
+export const todoAtom = atom({
+    key: 'todoAtom',
+    default: [
+        {
+            id: 1,
+            title: '우진이 사료 주문하기',
+            isDone: false,
+        },
+    ]
+})
