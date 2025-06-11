1b88abd119b5ff13dee8bde5d02b63f57cb1fd48
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-11 12:31:50 +0900

    RecoilTodo.jsx & atom.jsx 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import {createRoot} from 'react-dom/client'
 import './global.css'
 import RecoilCounter from "./RecoilCounter.jsx";
 import {RecoilRoot} from "recoil";
+import RecoilTodo from "./RecoilTodo.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
             <RecoilRoot>
-                <RecoilCounter />
+                <RecoilTodo />
             </RecoilRoot>
         </div>
     </>
