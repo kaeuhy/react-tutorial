31beb02851e1cac836ec56523f431712049433e0
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-16 19:41:25 +0900

    useDeferredValue 함수를 이용한 또 다른 예시

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import {createRoot} from 'react-dom/client'
 import './global.css'
 import HookTransition from "./HookTransition.jsx";
 import HookDefered from "./HookDeferred.jsx";
+import HookDeferredTransition from "./HookDeferredTransition.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
-            <HookDefered />
+            <HookDeferredTransition/>
         </div>
     </>
 );
