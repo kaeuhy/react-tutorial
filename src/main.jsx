4f09478ebe22e6de215f1d7fd83dfec6ca666e25
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-19 01:08:23 +0900

    루트 정의를 태그 형식으로 표현하기 위한 실습

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import {createRoot} from 'react-dom/client'
 import './global.css'
 import HookCustom from "./HookCustom.jsx";
 import NetworkHook from "./NetworkHook.jsx";
-import {RouterProvider} from "react-router-dom";
+import {Route, RouterProvider} from "react-router-dom";
 import routesBasic from "./routesBasic.jsx";
+import TopPage from "./TopPage.jsx";
+import ArticlePage from "./ArticlePage.jsx";
+import AboutPage from "./AboutPage.jsx";
 
 createRoot(document.getElementById('root')).render(
     <>
         <div className="centered">
-            <RouterProvider router={routesBasic}/>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/article" element={<ArticlePage />} />
+            <Route path="/about" element={<AboutPage />} />
         </div>
     </>
 );
