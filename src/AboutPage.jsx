95922fe2956c21612a2629ada475c49021b5af52
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-19 20:24:53 +0900

    mod

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function AboutPage() {
-  return (
-    <>
-      <p>About 페이지입니다.</p>
-    </>
-  );
+    const navigate = useNavigate();
+    const handleClick = () => navigate('/');
+
+    return (
+        <>
+            <p>About 페이지입니다.</p>
+            <button type="button" onClick={handleClick}>
+                홈으로 이동</button>
+        </>
+    );
 }
