ebdf890251be0e71665be08bb6ec5cbd87aa3062
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-19 16:45:58 +0900

    계층 관게를 나타내기위한 루트 테이블 routesLink.jsx 생성

---
+++ src/routesLink.jsx
@@ -0,0 +1,18 @@
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import RouterApp from "./RouterApp.jsx";
+import TopPage from "./TopPage.jsx";
+import ArticlePage from "./ArticlePage.jsx";
+import AboutPage from "./AboutPage.jsx";
+
+
+const routesLink = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RouterApp />}>
+            <Route path="" element={<TopPage />} />
+            <Route path="article" element={<ArticlePage />} />
+            <Route path="about" element={<AboutPage />} />
+        </Route>
+    )
+);
+
+export default routesLink;
