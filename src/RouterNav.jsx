882d0f09d14904d8791af1057a25d666ae18f8e5
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-19 19:30:21 +0900

    적용해야 할 스타일 클래스 변경 - clannName 속성

--- src/RouterNav.jsx
+++ src/RouterNav.jsx
@@ -1,14 +1,15 @@
 import {NavLink, Outlet} from "react-router-dom";
-import './RouterNav.css';
+
+const isCurrent = ({ isActive, isPending }) => isActive ? 'active' : '';
 
 
 export default function RouterNav() {
     return (
         <>
             <ul>
-                <li><NavLink to="/">홈</NavLink></li>
-                <li><NavLink to="/article">기사</NavLink></li>
-                <li><NavLink to="/about">회사 소개</NavLink></li>
+                <li><NavLink className={isCurrent} to="/">홈</NavLink></li>
+                <li><NavLink className={isCurrent} to="/article">기사</NavLink></li>
+                <li><NavLink className={isCurrent} to="/about">회사 소개</NavLink></li>
             </ul>
             <hr />
             <Outlet />
