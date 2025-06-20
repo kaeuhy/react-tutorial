c77c45a1a0ec15ab3d245a0abebed5bd45173df1
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-20 20:40:55 +0900

    Outlet Context를 통한 상위 루트에서 하위 루트로 값 전달

--- src/TopPage.jsx
+++ src/TopPage.jsx
@@ -1,3 +1,8 @@
+import { useEffect } from 'react';
+import { useOutletContext } from 'react-router-dom';
+
 export default function TopPage() {
+  const [, setCount] = useOutletContext();
+  useEffect(() => setCount(c => c + 1), [setCount]);
   return <p>홈페이지입니다.</p>;
 }
