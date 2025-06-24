7d0fe9f9d28e5cf94e923740b2f5d2ed0dab4840
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-24 17:04:23 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,4 +9,7 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 3000,
+  },
 });
