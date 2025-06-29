8ada2c15d1667f0be25ec11ebde1d66783e49ffd
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-29 22:50:34 +0900

    /book/form 루트에 BookFormPage 컴포넌트를 연결, 액션 함수를 정의

---
+++ src/BookFormPage.jsx
@@ -0,0 +1,23 @@
+import { Form } from 'react-router-dom';
+
+export default function BookFormPage() {
+  return (
+  <Form method="POST" noValidate>
+    <div>
+      <label htmlFor="title">제목:</label><br/>
+      <input id="title" name="title" type="text" size="20" />
+    </div>
+    <div>
+      <label htmlFor="price">가격:</label><br/>
+      <input id="price" name="price" type="number" />원
+    </div>
+    <div>
+      <label htmlFor="published">출간일:</label><br/>
+      <input id="published" name="published" type="date" />
+    </div>
+    <div>
+      <button type="submit">등록하기</button>
+    </div>
+  </Form>
+  );
+}
--- src/routesParam.jsx
+++ src/routesParam.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, json, Route } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, json, redirect } from 'react-router-dom';
 import RouterParam from './RouterParam';
 import TopPage from './TopPage';
 import BookPage from './BookPage';
@@ -8,8 +8,15 @@ import BookQueryPage from './BookQueryPage';
 import BookStatePage from './BookStatePage';
 import InvalidParamsPage from './InvalidParamsPage';
 import WeatherPage from './WeatherPage';
+import CommonErrorPage from './CommonErrorPage';
+import BookFormPage from './BookFormPage';
+import yup from './yup.kr';
+import { date, number, string } from 'yup';
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 const fetchWeather = async ({ params }) => {
+  await sleep(2000);
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=kr&appid=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`);
   if (res.ok) { return res; }
   switch (res.status) {
@@ -22,11 +29,35 @@ const fetchWeather = async ({ params }) => {
   }
 }
 
+const bookAction = async ({ request }) => {
+  const form = await request.formData();
+  const bookSchema = yup.object({
+    title: string().label('도서명').required().max(100),
+    price: number().label('가격').integer().positive(),
+    published: date().label('출간일').required().max(new Date(2100, 0, 1))
+  });
+  let info;
+  try {
+    info = await bookSchema.validate({
+      title: form.get('title'),
+      price: form.get('price') || 0,
+      published: new Date(form.get('published') || Date.now()),
+    }, {
+      abortEarly: false
+    });
+    console.log(info);
+    return redirect('/');
+  } catch (e) {
+    return e.errors;
+  }
+};
+
 const routesParam = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RouterParam />}
-      errorElement={<InvalidParamsPage />} >
+    <Route element={<RouterParam />} >
       <Route path="/" element={<TopPage />} />
+      <Route path="/book/form" element={<BookFormPage />}
+        action={bookAction} />
       <Route path="/book/:isbn?" element={<BookPage />} />
       <Route path="/bookQuery" element={<BookQueryPage />} />
       <Route path="/bookState" element={<BookStatePage />} />
