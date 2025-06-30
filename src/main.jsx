1151920156b00e0baad5d40cda6b24008621f2a2
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-30 16:52:24 +0900

    ScrollRestoration 요소를 통해 스크롤 관리

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client'
 import {Route, RouterProvider} from "react-router-dom";
-import routesMyLink from "./routesMyLink.jsx";
+import routesScroll from "./routesScroll.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <RouterProvider router={routesMyLink} />
+    <RouterProvider router={routesScroll} />
 );
