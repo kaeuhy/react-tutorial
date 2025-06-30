f5c159bb0f55f48b94ca3b00bdcd43809ea68f3e
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-30 15:10:21 +0900

    상위 컴포넌트의 하위 루트에 대응하는 컴포넌트 정의

--- src/routesParam.jsx
+++ src/routesParam.jsx
@@ -1,4 +1,5 @@
-import { Route, createBrowserRouter, createRoutesFromElements, json, redirect } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements,
+  json, redirect } from 'react-router-dom';
 import RouterParam from './RouterParam';
 import TopPage from './TopPage';
 import BookPage from './BookPage';
@@ -6,28 +7,12 @@ import SearchPage from './SearchPage';
 import NotFoundPage from './NotFoundPage';
 import BookQueryPage from './BookQueryPage';
 import BookStatePage from './BookStatePage';
-import InvalidParamsPage from './InvalidParamsPage.jsx';
+import InvalidParamsPage from './InvalidParamsPage';
 import WeatherPage from './WeatherPage';
 import CommonErrorPage from './CommonErrorPage';
 import BookFormPage from './BookFormPage';
 import yup from './yup.kr';
 import { date, number, string } from 'yup';
-
-const sleep = ms => new Promise(res => setTimeout(res, ms));
-
-const fetchWeather = async ({ params }) => {
-  await sleep(2000);
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=kr&appid=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`);
-  if (res.ok) { return res; }
-  switch (res.status) {
-    case 404:
-      throw json({ message: 'city is invalid!!' }, { status: 404 });
-    case 401:
-      throw json({ message: 'api key is invalid!!' }, { status: 401 });
-    default:
-      throw json({ message: 'api server is in trouble...' }, { status: 501 });
-  }
-}
 
 const bookAction = async ({ request }) => {
   const form = await request.formData();
@@ -62,8 +47,8 @@ const routesParam = createBrowserRouter(
       <Route path="/bookQuery" element={<BookQueryPage />} />
       <Route path="/bookState" element={<BookStatePage />} />
       <Route path="/search/*" element={<SearchPage />} />
-      <Route path="/weather/:city" element={<WeatherPage />}
-        loader={fetchWeather}/>
+      <Route path="/weather/:city"
+        lazy={() => import('./WeatherLazyPage')} />
       <Route path="*" element={<NotFoundPage />} />
       <Route path="/search/*" element={<SearchPage />} />
     </Route>
