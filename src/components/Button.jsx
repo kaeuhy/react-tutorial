4760605c200b5be8e8fc83a2e1bcc46e073b7358
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-20 23:45:40 +0900

    state & props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,15 @@ export default function Button({ children, text, color = "black" }) {
     // 이벤트 객체 e
     console.log(e);
     console.log(text, color);
-  }
+  };
 
   return (
     <button
       // 이벤트 핸들러
       onClick={onClickButton}
       // onMouseEnter={onClickButton}
-      style={{ color: color }}>
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,3 @@
 export default function Header() {
-  return (
-    <div>자식 컴포넌트</div>
-  );
+  return <div>자식 컴포넌트</div>;
 }
