5c1d67b5a97354c7565f066cc8f306391b78403f
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-21 00:21:20 +0900

    state를 사용한 입력 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import Counter from "./components/Counter.jsx";
-import Bulb from "./components/Bulb.jsx";
+import Register from "./components/Register.jsx";
 
 export default function App() {
-
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
---
+++ src/components/Register.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export default function Register() {
+  const [name, setName] = useState("이름");
+  const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeBirth = (e) => {
+    setBirth(e.target.value);
+  };
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const onChangeBio = (e) => {
+    setBio(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+      </div>
+
+      <div>
+        <input value={birth} onChange={onChangeBirth} type="date" />
+        {birth}
+      </div>
+
+      <div>
+        <select value={country} onChange={onChangeCountry}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+
+      <div>
+        <textarea value={bio} onChange={onChangeBio} />
+      </div>
+    </div>
+  );
+}
