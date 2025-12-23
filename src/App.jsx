d9e0c660aa5cf957ab1052b4fb312b48ec074f9c
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-24 00:16:50 +0900

    React Hooks & 커스텀 Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Register from "./components/Register.jsx";
+import HookExam from "./components/HookExam.jsx";
 
 export default function App() {
   return (
     <>
-      <Register />
+      <HookExam />
     </>
   );
 }
