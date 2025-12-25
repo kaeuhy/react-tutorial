ca12ebfbfff9dfde016948e2e6d6219468b73b34
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-25 21:32:34 +0900

    mockdate 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import './App.css';
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
+import { useState } from "react";
+import { mockDate } from "./items/items.js";
 
 export default function App() {
+  const [todos, setTodos] = useState(mockDate);
+
   return (
     <div className="App">
       <Header />
---
+++ src/items/items.js
@@ -0,0 +1,20 @@
+export const mockDate = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "Next 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "TypeScript 공부하기",
+    date: new Date().getTime(),
+  },
+];
