febdd9739d4cd0763ef8f3c7c27aee4c5f3d38d0
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-29 23:14:11 +0900

    투두리스트 투두 추가하기 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { mockDate } from "./items/items.js";
 
 export default function App() {
   const [todos, setTodos] = useState(mockDate);
+  const isRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: isRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,33 @@
 import "./../css/Editor.css";
+import { useRef, useState } from "react";
 
-export default function Editor() {
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onkeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return <div className="Editor">
-    <input placeholder="새로운 Todo..."/>
-    <button>
+    <input ref={contentRef} value={content} onKeyDown={onkeyDown} onChange={onChangeContent}
+           placeholder="새로운 Todo..." />
+    <button onClick={onSubmit}>
       추가
     </button>
   </div>;
