c647a6c3793e3822d0f2e83bf9f2d1f294792b81
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-31 12:07:53 +0900

    useMemo를 사용하여 연산 최적화

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./../css/List.css";
 import TodoItem from "./TodoItem.jsx";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 export default function List({ todos, onUpdate, onDelete }) {
   const [search, setSearch] = useState("");
@@ -18,15 +18,44 @@ export default function List({ todos, onUpdate, onDelete }) {
     });
   };
 
+  const {totalCount, doneCount, notDoneCount} = useMemo(()=>{
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    };}, [todos]);
+
   const filteredTodos = getFilteredData();
+
+  // const {totalCount, doneCount, notDoneCount} = getAnalyzedData()
 
   return (
     <div className="List">
       <h4>Todo List </h4>
-      <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete} />;
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
         })}
       </div>
     </div>
