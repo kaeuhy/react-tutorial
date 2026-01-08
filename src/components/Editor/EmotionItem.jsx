4edc4521c0dcac3c2d6bc99094be81e64e4882a7
Eunhyeon Kang <kaeuhy_0315@naver.com>
2026-01-08 17:34:29 +0900

    New.jsx 기능 구현

--- src/components/Editor/EmotionItem.jsx
+++ src/components/Editor/EmotionItem.jsx
@@ -1,9 +1,9 @@
 import "./EmotionItem.css";
 import { getEmotionImage } from "../../util/get-emotion-image.jsx";
 
-export default function EmotionItem({ emotionId, emotionName, isSeleted }) {
+export default function EmotionItem({ emotionId, emotionName, isSelected, onClick }) {
   return (
-    <div className={`EmotionItem ${isSeleted ? `EmotionItem_on_${emotionId}`: ""}`}>
+    <div onClick={onClick} className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}`: ""}`}>
       <img className="emotion_img" src={getEmotionImage(emotionId)} />
       <div className="emotion_name">{emotionName}</div>
     </div>
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,12 +1,22 @@
 import Header from "../components/Header/Header.jsx";
 import Button from "../components/Button/Button.jsx";
 import Editor from "../components/Editor/Editor.jsx";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App.jsx";
 
 export default function New() {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content); nav('/', {replace: true})
+  };
+
   return (
     <div>
-      <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로 가기"} />} />
-      <Editor />
+      <Header title={"새 일기 쓰기"} leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />} />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 }
